6e5dae19e3039021d5ba31cff5167d9aa0a76500
imyasars <yasarsona@gmail.com>
2024-11-08 00:15:05 +0530

    trained model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 from src.logger.logging import logging
 from src.exception.exception import customexception
-
-
 import os
 import sys
 from sklearn.model_selection import train_test_split
